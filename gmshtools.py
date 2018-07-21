dffa5b7838f4c65bcbff0bfd9a45e3f87cd7c75f
Ignacio Bejarano <ibejarano@localhost.localdomain>
2018-07-21 17:12:33 -0300

    Lector de gmsh implementado

---
+++ gmshtools.py
@@ -0,0 +1,30 @@
+
+def searchline(palabra, f ):
+    linea = f.readline()
+    condicion = (palabra in linea)
+    while not condicion:
+        linea = f.readline()
+        condicion = (palabra in linea)
+    if palabra in linea:
+        # found linea
+        print ("found: ", palabra) # " at linea: ", l
+        return linea, f
+    else:
+        print (palabra, ' not found')
+        return 0
+    return 0
+
+def readGmshFile(word,inpGmsh):
+    '''
+    INPUT
+    word: String con palabra a buscar
+    inpgmsh: Archivo en formato .gmsh donde busca word
+    OUTPUT
+    nitems: Cantidad de elementos hallados
+    Parser del file de Gmsh: Lista de listas con datos sobre el item necesario
+    '''
+    fileGmsh = open(inpGmsh,"r")
+    lines, fileGmsh = searchline(word,fileGmsh)
+    lines = fileGmsh.readline()
+    nitems = int(lines.split()[0])
+    return nitems , fileGmsh

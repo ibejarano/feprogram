15f7420fa11a429412aa9a4ac087dd8b02d0666b
Ignacio Bejarano <ibejarano@localhost.localdomain>
2018-07-22 21:31:24 -0300

    Esta casi todo, falta corregir el ensamblaje K ver print

--- elements.py
+++ elements.py
@@ -3,6 +3,7 @@ import scipy.sparse as sp
 import sys
 import logging
 from gmshtools import readGmshFile
+from scipy.sparse.linalg import spsolve
 
 def Constructor(entity,inpGmsh,cNodes):
     '''
@@ -19,9 +20,14 @@ def Constructor(entity,inpGmsh,cNodes):
         intLine = tuple(map(int,line.split()))
         if entity == '$Nodes':
             entityList.append(Node2D(intLine[1:4]))
-        else:
+        elif len(intLine) == 9:
             objNodes = nodesAsign(intLine[5:9],cNodes)
             entityList.append(ElemQ4(objNodes))
+        else:
+            physGroup = intLine[3]
+            intNode = intLine[6]
+            objNode = cNodes[intNode-1]
+            objNode.setBG(physGroup)
     return nEntity  , entityList
 
 def nodesAsign(tNodes,coordNodes):
@@ -33,6 +39,8 @@ def nodesAsign(tNodes,coordNodes):
     for i in tNodes:
         objNod.append(coordNodes[i-1])
     return objNod
+
+
 
 class ElemQ4:
     def __init__(self,nodes):
@@ -51,12 +59,6 @@ class ElemQ4:
         dhrs[0] = hr
         dhrs[1] = hs
         return dhrs
-
-    def funJ(self,r,s):
-        #FIXME: FUNCION NO IMPLEMENTADA, BORRAR SI NO SE VA A USAR
-        #J = np.matrix(np.zeros((2,2)))
-        #J = dHrs*self.Pos
-        return None
 
     def funB(self,dHrs,J):
         B = np.matrix(np.zeros((3,8)))
@@ -86,14 +88,46 @@ class Node2D:
     def __init__(self,coords):
         self.x = coords[0]
         self.y = coords[1]
-        self.DIR = False
+        self.DIRx = False
+        self.DIRy = False
         self.NEU = False
         self.nglob = self.nglob
+        self.BG = False
         Node2D.nglob += 1
-    def setDIR(self):
-        self.DIR = True
+
+    def setBG(self, group):
+        self.BG = True
+        self.bcGroup = group
+
     def setNEU(self):
         self.NEU = True
+
+    def setDIRxValue(self,value):
+        self.DIRx = True
+        self.xValue = value
+
+    def setDIRyValue(self,value):
+        self.DIRy = True
+        self.yValue = value
+
+    def storeCalcValue(self,xValue,yValue):
+        self.xValue = xValue
+        self.yValue = yValue
+
+    def physGrouptoValue(self,bclist):
+        '''
+        Pasar los numeros del phys group a un valor
+        '''
+        group = self.bcGroup - 1
+        pairValue = bclist[group]
+        aux = 0
+        for bc in pairValue:
+            if bc != None:
+                if aux == 0:
+                    self.setDIRxValue(bc)
+                else:
+                    self.setDIRyValue(bc)
+            aux += 1
 
 def funHrs(r,s):
     #FIXME: FUNCION NO IMPLEMENTADA, CONTIENE LAS FUNCIONES DE INTERPOLACION
@@ -104,20 +138,34 @@ def funHrs(r,s):
     hrs = np.matrix([h1,h2,h3,h4])
     return hrs
 
-def Assemble(elem,Ke,K):
+def Assemble(elem,Ke,K,brhs):
     '''
     elem: Objeto elemento
     Ke: Matriz de rigidez local a ensamblar en global
     K: Matriz global
+    gl: grado de libertad = nnodos x 2, es el indice de la K global
     '''
     for i in range(4):
-        ni = elem.nloc[i].nglob
-        for j in range(4):
-            nj = elem.nloc[j].nglob
-            K[ni-1,nj-1] += Ke[i,j]
-    return K
-
-
+        ni = elem.nloc[i].nglob-1
+        gl = ni*2
+        if elem.nloc[i].DIRx and elem.nloc[i].DIRy :
+            K[gl,gl:gl+2] = 1
+            brhs[gl:gl+2] = elem.nloc[i].xValue
+            brhs[gl+1] = elem.nloc[i].yValue
+            test = [elem.nloc[i].xValue , elem.nloc[i].yValue]
+            print(K[gl,:])
+        elif elem.nloc[i].DIRx:
+            K[gl,gl] = 1
+            brhs[gl] = elem.nloc[i].xValue
+        elif elem.nloc[i].DIRy:
+            K[gl+1,gl+1] = 1
+            brhs[gl+1] = elem.nloc[i].yValue
+        else :
+            for j in range(4):
+                nj = elem.nloc[j].nglob
+                glj= nj*2
+                K[gl,glj:glj+2] += Ke[i*2,j*2:j*2+1]
+    return K , brhs
 
 fileGmsh = sys.argv[1]
 print(fileGmsh)
@@ -125,4 +173,23 @@ n, coord = Constructor('$Nodes',fileGmsh,None)
 nelem, conect = Constructor('$Elements',fileGmsh,coord)
 
 #Inicio de matriz global
-K = sp.lil_matrix((n,n))
+K = sp.lil_matrix((n*2,n*2))
+brhs = sp.lil_matrix((n*2,1))
+#Formato de cond de borde
+bcList = [[0,0],[1,None],[0,0],[None,1]]
+
+for nodin in coord:
+    if nodin.BG:
+        nodin.physGrouptoValue(bcList)
+
+for elemtest in conect:
+    Ke = elemtest.getKe()
+    K , bhrs = Assemble(elemtest, Ke , K , brhs)
+
+print(K)
+
+#K = K.tocsc
+#brhs = brhs.tocsc
+
+#U = spsolve(K,brhs)
+#print(U)

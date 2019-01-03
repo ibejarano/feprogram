244957d2898e937729d308bdf5f3c6bdcd421f78
Ignacio Bejarano <ignbejarano@gmail.com>
2019-01-03 15:40:45 -0300

    fixed quad4

--- elements.py
+++ elements.py
@@ -39,11 +39,7 @@ def Constructor(entity,inpGmsh,cNodes,bc=False):
                 entityList = readElements(entityFile,entityList,'Q9',cNodes)
 
             else:
-                while (line != '$EndElements\n'):
-                    intLine = tuple(map(int,line.split()))
-                    objNodes = nodesAsign(intLine[5:len(intLine)],cNodes)
-                    entityList.append(Elem(objNodes,'Q4'))
-                    line = entityFile.readline()  
+                entityList = readElements(entityFile,entityList,'Q4',cNodes)
             nEntity -=1
     return nEntity  , entityList
 
@@ -60,7 +56,7 @@ def readElements(fileGmsh,conectivity,elemType,coords):
     while (line != '$EndElements\n'):
         intLine = tuple(map(int,line.split()))
         objNodes = nodesAsign(intLine[5:len(intLine)],coords)
-        conectivity.append(Elem(objNodes,'Q9')) #FIXME : corregir que no asigne solo a Q4
+        conectivity.append(Elem(objNodes,elemType))
         line = fileGmsh.readline() 
 
     return conectivity
@@ -194,7 +190,8 @@ class Elem:
 
 
     def funB(self,dHrs,J):
-        B = np.matrix(np.zeros((3,18)))
+        dof = len(self.nloc)*2
+        B = np.matrix(np.zeros((3,dof)))
         Jinv = np.linalg.inv(J)
         Dh = Jinv * dHrs
         for i in range(2):
@@ -204,7 +201,8 @@ class Elem:
 
     def getpos(self):
         auxlist = []
-        for i in range(9):
+        nNodeslocal = len(self.nloc)
+        for i in range(nNodeslocal):
             auxlist.append([self.nloc[i].x,self.nloc[i].y])
         return np.matrix(auxlist)   
 
@@ -214,10 +212,11 @@ class Elem:
         return Hrs[gps] * pos
     # @profile
     def getKe(self,H,Hrs,C):
+        dof = len(self.nloc)*2
         J = np.matrix(np.zeros((2,2)))
-        Ke = np.matrix(np.zeros((18,18)))
-        Be = np.matrix(np.zeros((3,18)))
-        for i in range(9):
+        Ke = np.matrix(np.zeros((dof,dof)))
+        Be = np.matrix(np.zeros((3,dof)))
+        for i in range(int(dof/2)):
             J = self.calcJacobian(i,Hrs)
             detJ = np.linalg.det(J)
             B = self.funB(Hrs[i],J)
@@ -285,9 +284,10 @@ class FemProblem:
             self.Hrs = self.quadHrs(gpsList,self.funHrs9,gpsWei)
         
         elif self.elemType == 'Quad4':
-            gpsList = [-0.5777,0.5777] 
-            self.H = self.quadH(gpsList,self.funH4)
-            self.Hrs = self.quadHrs(gpsList,self.funHrs4)
+            gpsList = [-0.5777,0.5777]
+            gpsWei = [1,1]
+            self.H = self.quadH(gpsList,self.funH4,gpsWei)
+            self.Hrs = self.quadHrs(gpsList,self.funHrs4,gpsWei)
 
         else:
             raise Exception('Invalid elemType defined you must use Quad4 or Quad9')
@@ -302,6 +302,20 @@ class FemProblem:
         return H
 
     def quadHrs(self,gpoints,formFunction,gpweights=None):
+        '''Creates an array that has the derivatives of form
+        function evaluated at gauss points
+        
+        Arguments:
+            gpoints {list[float]} -- List with gauss points
+            formFunction {callback} -- returns the gp evaluated
+        
+        Keyword Arguments:
+            gpweights {list[float]} -- it weight numerical integration (default: {None})
+        
+        Returns:
+            He [array] -- contains the functions evaluated at gausspoints and ordered
+        '''
+
         He = []
         for weir , gpr in enumerate(gpoints):
             for weis, gps in enumerate(gpoints):
@@ -394,9 +408,10 @@ class FemProblem:
         '''
         Optimized one
         '''
+        dof = len(elem.nloc)*2
         listedNodes = [(x-1)*2+y for x in elem.localNodes() for y in [0,1]]
-        col = listedNodes*18
-        row = [ind for ind in listedNodes for i in range(18)]
+        col = listedNodes*dof
+        row = [ind for ind in listedNodes for i in range(dof)]
         Kelem = Ke.ravel()
         return row , col , Kelem
 
